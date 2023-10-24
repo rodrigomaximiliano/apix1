d53668b9ce8cc4b8b005ffb2c38aa12e331e66bc
Rodrigo Maximiliano <maxilcksly@gmail.com>
2023-10-24 13:08:52 -0300

    Implementacion de rutas

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { librosController } from './controller.js';
 
+
+
 const router = Router();
 
 router.get('/libros', librosController.getAll);
 router.get('/libros/:id', librosController.getOne);
-router.post('/libros', librosController.create); // Ruta para crear un libro
+
 router.put('/libros/:id', librosController.update); // Ruta para actualizar un libro
 router.delete('/libros/isbn/:isbn', librosController.deleteByISBN); // Ruta para eliminar un libro por ISBN
 
