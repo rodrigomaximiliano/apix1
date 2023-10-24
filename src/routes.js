ecf799bbfdd6a046797040658c276ed03fb68e92
Rodrigo Maximiliano <maxilcksly@gmail.com>
2023-10-24 16:47:46 -0300

    Implementación de nuevas rutas

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import { librosController } from './controller.js';
 
 
 
-const router = Router();
 
 router.get('/libros', librosController.getAll);
 router.get('/libros/:id', librosController.getOne);
